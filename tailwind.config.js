955cc6be0a475dbc47c1c1224e0e9092610a94ca
Brunno Hibbeln <brunnohibbeln10@gmail.com>
2023-09-04 10:22:46 -0300

    initial `configs`

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,25 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-pink': 'hsl(6, 100%, 80%)',
+        'dark-pink': 'hsl(335, 100%, 65%)',
+
+        'pale-blue': 'hsl(243, 100%, 93%)',
+        'grayish-blue': 'hsl(229, 7%, 55%)',
+        blue: 'hsl(228, 56%, 26%)',
+        'dark-blue': 'hsl(229, 57%, 11%)',
+      },
+      fontFamily: {
+        raleway: ['var(--font-raleway)'],
+      },
+      fontSize: '0.875rem',
+      backgroundImage: {
+        mobile: ['url(./images/bg-mobile.png)'],
+        desktop: ['url(./images/bg-desktop.png)'],
+      },
+    },
   },
   plugins: [],
 }
